dffdbca7c98a5cd07e84ac2276f0684812c0868e
Yui <senkowo@mailbox.org>
2023-12-25 19:28:07 -0500

    only demo in here, should be working...

--- my-packages/renoise.scm
+++ my-packages/renoise.scm
@@ -16,6 +16,9 @@
   #:use-module (ice-9 regex) ; match syntax
   #:use-module (srfi srfi-1) ; 'hidden?
   #:use-module (guix gexp) ; replace 'install
+  ;; transformations
+  #:use-module (guix transformations) ; options->transformation
+  #:use-module (guix profiles) ; packages->manifest
   ;; #:use-module (guix licenses)
   #:use-module ((nonguix licenses) #:prefix license:)
   #:use-module (nonguix build-system binary) ; for binary-build-system
@@ -27,7 +30,6 @@
   ;; #:use-module (srfi srfi-34)
   ;; #:use-module (guix i18n)
   ;; #:use-module (guix build utils)
-  ;; #:use-module (guix )
   )
 
 ;; references:
@@ -58,49 +60,31 @@
     ("aarch64-linux" "arm64")
     ("armhf-linux" "armhf")))
 
-;; da real renoise package
-(define-public renoise-test
+;; renoise demo ver
+(define-public renoise-demo
   (package
-   (name "renoise")
+   (name "renoise-demo")
    (version "3.4.3")
    (source
-    ;; (local-file renoise-source-path #:recursive #t)
-    ;; can be just a (local-file "path" #:recursive #t/f?) instead?
-    (cond (renoise-source-path
-           ;; If a local path to the renoise tar file is specified, it will install
-           ;; that. (Note that the following adds the tar file to the guix store, so
-           ;; that it can be processed) (Also note that each paid renoise tarbomb has
-           ;; a unique hash, so it cannot be predefined.)
-           ;; (let ((tarball (with-store store
-           ;;                            (download-to-store store renoise-source-path))))
-           ;;   (origin
-           ;;    (method url-fetch)
-           ;;    (uri renoise-source-path)
-           ;;    (sha256
-           ;;     (base32 (bytevector->nix-base32-string
-           ;;              (file-hash* tarball #:recursive? #false))))))
-	   (local-file renoise-source-path #:recursive #t)
-	   )
-          (t
-           ;; If renoise-source-path is not defined, download and install the demo ver
-           ;; from the site:
-           ;; e.g. https://files.renoise.com/demo/Renoise_3_4_3_Demo_Linux_x86_64.tar.gz
-           (let ((def-uri
-                   (string-append
-                    "https://files.renoise.com/demo/Renoise_"
-                    (string-replace-substring version "." "_")
-                    "_Demo_Linux_"
-                    (get-current-system)
-                    ".tar.gz")))
-             (origin
-              (method url-fetch/tarbomb)
-              (uri def-uri)
-              (sha256
-               (base32
-                (match (get-current-system) ; i might not keep the hashes up to date, perchance.
-                  ("x86_64" "1hd7fbk0px45fxhqa7nqcnij8ls2fhpjp60v840vy2zqs9fkcr52")
-                  ("arm64" "0zpkaiwwxn8yh3s1d22qswshbgaxx5d8iy17hb3w256zgb722yjw")
-                  ("armhf" "18174b1lgsk73gxhala471ppzbrpa1cs953b5par998yqgh74znk")))))))))
+    ;; If renoise-source-path is not defined, download and install the demo ver
+    ;; from the site:
+    ;; e.g. https://files.renoise.com/demo/Renoise_3_4_3_Demo_Linux_x86_64.tar.gz
+    (let ((def-uri
+            (string-append
+             "https://files.renoise.com/demo/Renoise_"
+             (string-replace-substring version "." "_")
+             "_Demo_Linux_"
+             (get-current-system)
+             ".tar.gz")))
+      (origin
+       (method url-fetch/tarbomb)
+       (uri def-uri)
+       (sha256
+        (base32
+         (match (get-current-system) ; i might not keep the hashes up to date, perchance.
+           ("x86_64" "9hd7fbk0px45fxhqa7nqcnij8ls2fhpjp60v840vy2zqs9fkcr52")
+           ("arm64" "0zpkaiwwxn8yh3s1d22qswshbgaxx5d8iy17hb3w256zgb722yjw")
+           ("armhf" "18174b1lgsk73gxhala471ppzbrpa1cs953b5par998yqgh74znk")))))))
    (build-system binary-build-system)
    (arguments
     (append
@@ -170,100 +154,7 @@
                             (invoke "sh" "./install.sh")
                             (format #T "> Finished running install script...~%")
 
-                            ;; (invoke "ls" "-al" "/sys")
-                            ;; (invoke "ls" "-al" "/proc")
-                            ;; (invoke "grep" "-E" "'performance'"
-                            ;;     "/sys/devices/system/cpu/cpu*/cpufreq/scaling_governor"
-                            ;;     ">" "/dev/null")
-                            
-                            ;; (invoke "ls" "-al" target)
-                            
-                            ;; (error "made-it!!")
-                            
-                            ;; ---------- Method 2 -----------
-
-                            ;; ;; Fixing file permissions...
-                            ;; (format #T "> Fixing file permissions...~%")
-                            
-                            ;; ;; wrapper around find-files (similar to "$ find A -type B
-                            ;; ;; -name C -exec chmod D {} \;")
-                            ;; (define (find-and-chmod path type name-regex ch-perm)
-                            ;;   (for-each (lambda (f)
-                            ;;           (chmod f ch-perm))
-                            ;;         (find-files
-                            ;;          path 
-                            ;;          (lambda (file stat) ; checks type and name-regex:
-                            ;;            (and
-                            ;;             (cond ((equal? "d" type)
-                            ;;                    (directory-exists? file))
-                            ;;                   ((equal? "f" type)
-                            ;;                    (not (directory-exists? file)))
-                            ;;                   (t (error "invalid find-command type")))
-                            ;;             ((file-name-predicate name-regex) file stat)))
-                            ;;          #:directories? #t
-                            ;;          #:fail-on-error? #t)))
-                            
-                            ;; (find-and-chmod "." "d" ".*" '#o755)
-                            ;; (find-and-chmod "." "f" ".*" '#o644)
-                            ;; (find-and-chmod "." "f" ".*sh" '#o755)
-                            ;; (find-and-chmod "./Installer/xdg-utils" "f" "xdg-.*" '#o755)
-                            ;; (chmod "./renoise" '#o755)
-                            ;; (find-and-chmod "./Resources" "f" "AudioPluginServer_.*" '#o755)
-                            
-                            ;; ;; Installing shared resources...
-                            ;; (format #T "> Installing shared resources...~%")
-                            ;; (mkdir-p resources) ; vvv does not copy the src dir itself
-                            ;; (copy-recursively "./Resources" resources)
-                            ;; (install-file "./install.sh" resources)
-                            ;; (install-file "./uninstall.sh" resources)
-                            ;; (copy-recursively "./Installer"
-                            ;;               (string-append resources "/Installer"))
-                            
-                            ;; ;; Installing the executable...
-                            ;; (format #T "> Installing the executable...~%")
-                            ;; (mkdir-p bin)
-                            ;; (copy-file "./renoise" ; dont install-file, want a different name
-                            ;;        (string-append bin "/renoise-" #$version))
-                            
-                            ;; ;; Linking the executable...
-                            ;; (format #T "> Linking the executable...~%")
-                            ;; (symlink (string-append bin "/renoise-" #$version)
-                            ;;      (string-append bin "/renoise"))
-
-                            ;; ;; Installing the man file...
-                            ;; (format #T "> Installing the man file...~%")
-                            ;; (install-file "./Installer/renoise.1.gz"
-                            ;;           (string-append share "/man/man1"
-                            ;;                          "/renoise.1.gz"))
-                            ;; (install-file "./Installer/renoise-pattern-effects.5.gz"
-                            ;;           (string-append share "/man/man5"
-                            ;;                          "/renoise-pattern-effects.5.gz"))
-                            
-                            ;; ;; Registering MIME types... ; what's this do? location?
-                            ;; (format #T "> Registering MIME types...~%")
-                            ;; (invoke "xdg-mime" "install" "--novendor"
-                            ;;     (string-append resources "/Installer/renoise.xml"))
-
-                            ;; ;; Installing icons...
-                            ;; (format #T "> Installing icons...~%")
-                            ;; (for-each (lambda (res) 
-                            ;;         (install-file
-                            ;;          (string-append resources "/Installer/renoise-" res
-                            ;;                         ".png")
-                            ;;          (string-append share "/icons/hicolor/"
-                            ;;                         res "x" res
-                            ;;                         "/apps/renoise.png")))
-                            ;;       '("48" "64" "128"))
-                            
-                            ;; ;; Installing desktop-menu shortcuts...
-                            ;; (format #T "> Installing desktop-menu shortcuts...~%")
-                            ;; (invoke "xdg-desktop-menu" "install" "--novendor"
-                            ;;     (string-append resources "/Installer/renoise.desktop"))
-                            
-                            ;; ;; Do checks here?
                             )))))))
-   
-   
    (native-inputs
     (list
      ;; grep
@@ -285,6 +176,37 @@
    (home-page "https://www.renoise.com/")
    (license (license:nonfree (string-append "file:///share/doc/renoise-" version
                                             "/License.txt")))))
+
+;; just for testing
+(define-public paid-renoise-ver-manifest
+  (define renoise
+    (package
+     (inherit renoise-demo)
+     (name "renoise")))
+  (define transform-install-path
+    (options->transformation
+     '((with-source
+	. "renoise=/home/yui/Music/prod/misc/test/Renoise_3_4_3_Demo_Linux_x86_64.tar.gz"))))
+  (packages->manifest (list (transform-install-path renoise))))
+
+paid-renoise-ver-manifest
+
+
+;; (local-file renoise-source-path #:recursive #t)
+;; can be just a (local-file "path" #:recursive #t/f?) instead?
+;; If a local path to the renoise tar file is specified, it will install
+;; that. (Note that the following adds the tar file to the guix store, so
+;; that it can be processed) (Also note that each paid renoise tarbomb has
+;; a unique hash, so it cannot be predefined.)
+;; (let ((tarball (with-store store
+;;                            (download-to-store store renoise-source-path))))
+;;   (origin
+;;    (method url-fetch)
+;;    (uri renoise-source-path)
+;;    (sha256
+;;     (base32 (bytevector->nix-base32-string
+;;              (file-hash* tarball #:recursive? #false))))))
+;; )
 
 ;; run "gcc-unhidden:lib" to get the libs
 ;; (define-public gcc-unhidden
