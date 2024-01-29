a35eb289f90e3b411e7485d3dba2debbfdff419d
Yui <94131088+senkowo@users.noreply.github.com>
2024-01-29 01:14:04 -0500

    some notes

--- yumi/packages/cargo.scm
+++ yumi/packages/cargo.scm
@@ -10,6 +10,8 @@
 ;; TODO: figure out a way to not have to specify version when
 ;;  inheriting packages from crates-io and etc.
 ;; TODO: use paths in repo for #:cargo-inputs
+;; TODO: inherit dependencies and adjust their #:cargo-build-flags to
+;; have "--features pkgname"?
 
 (define-public hearth-0.0
   (let ((commit "90eb95f62a32981cb662b0f750027231d8a2586b"))
@@ -29,30 +31,30 @@
           (base32 "1w503n9rxja3h8ls6p5xsly8aclbp30dm4hd0525bvpbippi161v"))))
       (build-system cargo-build-system)
       (arguments
-       `(#:cargo-test-flags
-         (list "--release")
-         #:cargo-inputs
-         (("rust-glam" ,rust-glam-0.20)         ; features: bytemuck,serde
-          ("rust-bytemuck" ,rust-bytemuck-1.13) ;features: derive
+       `(#:cargo-inputs
+         (("rust-bytemuck" ,rust-bytemuck-1.13) ;features: derive
+          ("rust-glam" ,rust-glam-0.20)         ; features: bytemuck,serde
           ("rust-flume" ,rust-flume-0.11)
-          ;;; TOFIX
-          ;; ("hearth-canvas.path" "plugins/canvas")
-          ;; ("hearth-canvas.path" "plugins/canvas")
-          ;; ("hearth-daemon.path" "plugins/daemon")
-          ;; ("hearth-debug-draw.path" "plugins/debug-draw")
-          ;; ("hearth-init.path" "plugins/init")
-          ;; ("hearth-ipc.path" "core/ipc")
-          ;; ("hearth-fs.path" "plugins/fs")
-          ;; ("hearth-macros.path" "core/macros")
-          ;; ("hearth-network.path" "plugins/network")
-          ;; ("hearth-rend3.path" "plugins/rend3")
-          ;; ("hearth-renderer.path" "plugins/renderer")
-          ;; ("hearth-runtime.path" "core/runtime")
-          ;; ("hearth-schema.path" "core/schema")
-          ;; ("hearth-terminal.path" "plugins/terminal")
-          ;; ("hearth-time.path" "plugins/time")
-          ;; ("hearth-wasm.path" "plugins/wasm")
-          ;; ("kindling-build.path" "kindling/build")
+          
+          ;; TOFIX (how to fix?)
+          ("hearth-canvas.path" "plugins/canvas")
+          ("hearth-canvas.path" "plugins/canvas")
+          ("hearth-daemon.path" "plugins/daemon")
+          ("hearth-debug-draw.path" "plugins/debug-draw")
+          ("hearth-init.path" "plugins/init")
+          ("hearth-ipc.path" "core/ipc")
+          ("hearth-fs.path" "plugins/fs")
+          ("hearth-macros.path" "core/macros")
+          ("hearth-network.path" "plugins/network")
+          ("hearth-rend3.path" "plugins/rend3")
+          ("hearth-renderer.path" "plugins/renderer")
+          ("hearth-runtime.path" "core/runtime")
+          ("hearth-schema.path" "core/schema")
+          ("hearth-terminal.path" "plugins/terminal")
+          ("hearth-time.path" "plugins/time")
+          ("hearth-wasm.path" "plugins/wasm")
+          ("kindling-build.path" "kindling/build")
+          
           ("rust-ouroboros" ,rust-ouroboros-0.18)
           ("rust-parking_lot" ,rust-parking-lot-0.12)
           ("rust-serde_json" ,rust-serde-json-1) ; TOFIX default-features=f, features:derive
